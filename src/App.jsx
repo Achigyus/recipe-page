e0f5cb21df9aeec13fabb0a2d5384af7be0af78e
Achigyus <70823478+Achigyus@users.noreply.github.com>
2024-06-08 01:03:04 +0100

    finished mobile styles

--- src/App.jsx
+++ src/App.jsx
@@ -43,49 +43,49 @@ function App() {
           <div className="recipe_page_instructions">
             <h2 className="recipe_page_instructions_title young-serif-regular">Instructions</h2>
             <ol className="recipe_page_instructions_list">
-              <li className="recipe_page_instructions_list_item">Beat the eggs: In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed.
+              <li className="recipe_page_instructions_list_item outfit-400"><span>Beat the eggs</span>: In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed.
                 You can add a tablespoon of water or milk for a fluffier texture.</li>
-              <li className="recipe_page_instructions_list_item">Heat the pan: Place a non-stick frying pan over medium heat and add butter or oil.</li>
-              <li className="recipe_page_instructions_list_item">Cook the omelette: Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure
+              <li className="recipe_page_instructions_list_item outfit-400"><span>Heat the pan</span>: Place a non-stick frying pan over medium heat and add butter or oil.</li>
+              <li className="recipe_page_instructions_list_item outfit-400"><span>Cook the omelette</span>: Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure
                 the eggs evenly coat the surface.</li>
-              <li className="recipe_page_instructions_list_item">Add fillings (optional): When the eggs begin to set at the edges but are still slightly runny in the
+              <li className="recipe_page_instructions_list_item outfit-400"><span>Add fillings (optional)</span>: When the eggs begin to set at the edges but are still slightly runny in the
                 middle, sprinkle your chosen fillings over one half of the omelette.</li>
-              <li className="recipe_page_instructions_list_item">Fold and serve: As the omelette continues to cook, carefully lift one edge and fold it over the
+              <li className="recipe_page_instructions_list_item outfit-400"><span>Fold and serve</span>: As the omelette continues to cook, carefully lift one edge and fold it over the
                 fillings. Let it cook for another minute, then slide it onto a plate.</li>
-              <li className="recipe_page_instructions_list_item">Enjoy: Serve hot, with additional salt and pepper if needed.</li>
+              <li className="recipe_page_instructions_list_item outfit-400"><span>Enjoy</span>: Serve hot, with additional salt and pepper if needed.</li>
             </ol>
           </div>
 
           <hr />
 
           <div className="recipe_page_nutrition">
-            <h2 className="recipe_page_nutrition_title">Nutrition</h2>
-            <p className="recipe_page_nutrition_info">The table below shows nutritional values per serving without the additional fillings.</p>
+            <h2 className="recipe_page_nutrition_title young-serif-regular">Nutrition</h2>
+            <p className="recipe_page_nutrition_info outfit-400">The table below shows nutritional values per serving without the additional fillings.</p>
             <div className="recipe_page_nutrition_breakdown">
               <div className="recipe_page_nutrition_breakdown_item">
-                <p className="recipe_page_nutrition_nutrient">Calories</p>
-                <p className="recipe_page_nutrition_nutrient_amount">277kcal</p>
+                <p className="recipe_page_nutrition_nutrient outfit-400">Calories</p>
+                <p className="recipe_page_nutrition_nutrient_amount outfit-700">277kcal</p>
               </div>
 
               <hr />
 
               <div className="recipe_page_nutrition_breakdown_item">
-                <p className="recipe_page_nutrition_nutrient">Carbs</p>
-                <p className="recipe_page_nutrition_nutrient_amount">0g</p>
+                <p className="recipe_page_nutrition_nutrient outfit-400">Carbs</p>
+                <p className="recipe_page_nutrition_nutrient_amount outfit-700">0g</p>
               </div>
 
               <hr />
 
               <div className="recipe_page_nutrition_breakdown_item">
-                <p className="recipe_page_nutrition_nutrient">Protein</p>
-                <p className="recipe_page_nutrition_nutrient_amount">20g</p>
+                <p className="recipe_page_nutrition_nutrient outfit-400">Protein</p>
+                <p className="recipe_page_nutrition_nutrient_amount outfit-700">20g</p>
               </div>
 
               <hr />
 
               <div className="recipe_page_nutrition_breakdown_item">
-                <p className="recipe_page_nutrition_nutrient">Fat</p>
-                <p className="recipe_page_nutrition_nutrient_amount">22g</p>
+                <p className="recipe_page_nutrition_nutrient outfit-400">Fat</p>
+                <p className="recipe_page_nutrition_nutrient_amount outfit-700">22g</p>
               </div>
             </div>
           </div>
